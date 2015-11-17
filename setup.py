82fdf28be55c28d5b7a2fa5a4db00928a40e13c2
Matt Mulholland <mulhodm@gmail.com>
2015-11-17 00:04:41 -0500

    Commit setup scripts, etc.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from os.path import join
+from subprocess import getoutput
+from setuptools import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+# Hackish way of doing this. Find better way...
+root_env = getoutput('conda info | grep "package cache :"'
+                     ' | awk \'{print $4}\'')
+python_header_dir = join(root_env, 'python-3.4.3-2/include/python3.4m')
+
+setup(name = 'spaCy and Cython demo',
+      description='Repository storing spaCy demo + mini-Cython demo.',
+      url='https://github.com/mulhod/spaCy_demo',
+      author='Matt Mulholland',
+      author_email='mulhodm@gmail.com',
+      packages=['awesome_hello_world'],
+      cmdclass={'build_ext': build_ext},
+      ext_modules=[Extension('awesome_hello_world.hello',
+                             ['awesome_hello_world/hello.pyx'],
+                             include_dirs=[python_header_dir])],
+      keywords='spacy cython',
+      classifiers=['Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'License :: MIT',
+                   'Programming Language :: Python :: 3.4',
+                   'Topic :: Scientific/Engineering',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS'],
+      zip_safe=True)
---
+++ setup.sh
@@ -0,0 +1,63 @@
+#!/bin/zsh
+
+# Usage: ./setup.sh
+
+set -eu
+
+ORIG_DIR=$(pwd)
+THIS_DIR=$(dirname $(readlink -f $0))
+cd ${THIS_DIR}
+
+echo "Make sure that conda (miniconda) is installed before trying to set up" \
+     "or else this script will fail...\n"
+
+echo "Creating conda environment...\n"
+# Not sure if this is necessary
+conda config --add channels pypi
+conda config --add channels https://conda.anaconda.org/spacy
+# Create environment first and force python=3.4 (for some reason, just
+# adding python=3.4 to the list of packages in conda_requirements.txt
+# does not work as it is not recognized as a valid package name)
+conda create --yes -n spacy python=3.4
+# And now install all of the packages we need
+source activate spacy
+conda install --yes --file conda_reqs.txt
+if [[ $? -gt 0 ]]; then
+    
+    echo "\"conda install --yes --file conda_reqs.txt\" failed. Exiting.\n"
+    cd ${ORIG_DIR}
+    exit 1
+    
+fi
+echo "Created \"spacy\" environment successfully! To use environment, run" \
+     "\"source activate spacy\". To get out of the environment, run" \
+     "\"source deactivate\".\n"
+
+echo "Installing some extra packages with pip (since conda does not seem to" \
+     "want to install them)...\n"
+pip install pudb
+if [[ $? -gt 0 ]]; then
+    
+    echo "pip installation of pudb failed. Exiting.\n"
+    cd ${ORIG_DIR}
+    exit 1
+    
+fi
+
+# Download model data for spaCy
+echo "Downloading model data for spaCy package...\n"
+python3.4 -m spacy.en.download all
+
+# Compile Cython modules
+echo "Compiling Cython extensions...\n"
+python3.4 setup.py develop
+echo "Package installed!"
+echo "If changes are made to the Cython extensions, run the following to" \
+     "re-compile the extensions for use in the various command-line" \
+     "utilities:\n\tpython setup.py build_ext\n\nOr run the following to" \
+     "reinstall the entire package:\n\tpython setup.py install\n\nOr run" \
+     "the following to reinstall the entire package in the current" \
+     "location, i.e., in development mode:\n\tpython setup.py develop\n"
+
+echo "Setup complete. Use \"source activate spacy\" to activate conda" \
+     "environment.\n"
